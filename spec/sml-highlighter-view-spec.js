c58cb94b9147a15703b784e28d84199fad7e235b
Li You <liyouvane@gmail.com>
2016-11-24 00:12:16 -0500

    init

---
+++ spec/sml-highlighter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SmlHighlighterView from '../lib/sml-highlighter-view';
+
+describe('SmlHighlighterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
